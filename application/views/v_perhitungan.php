54af262c0f3726ecb3d96720ee09225721888c5c
Acmad Fadli Aditama <37434467+fadliaditama@users.noreply.github.com>
2019-11-29 20:06:37 +0700

    Update v_perhitungan.php
    Tambah 6 Tabel aspek, gap, pembobotan, perhitungan factor (second dan core), perhitungan nilai total, dan terakhir tabel ranking

--- application/views/v_perhitungan.php
+++ application/views/v_perhitungan.php
@@ -1,45 +1,314 @@
 <?php $this->load->view('layout/header');?>
 	<div class="container"> 
-		<h1>Alternatif</h1>
+		<h1>Perhitungan</h1>
+		<br>
 		<div>
-			<form method="POST" action="insert">
-			  <div class="form-group col-md-3">
-			    <label for="exampleInputEmail1">Kode Alternatif</label>
-			    <input type="kode" class="form-control" id="" placeholder="Contoh A1">
-			  </div>
-			  <div class="form-group col-md-3">
-			    <label for="exampleInputPassword1">Nama Alternatif</label>
-			    <input type="password" class="form-control" id="" placeholder="Contoh Alternatif 1">
-			  </div>
-			  <div class="col-md-3">
-			  	<button type="submit" class="btn btn-primary">Tambah</button>
-			  </div>
-			</form>
+			<h2>Tabel Aspek Kandungan Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 		<br>
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>no</th>
-					<th>Nim</th>
-					<th>Nama</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				$no = 1;
-				foreach ($mahasiswa as $row) 
-				{ ?>
+		<div>
+			<h2>Tabel Aspek Kandungan Non Asam</h2>
+			<table class="table table-bordered">
+				<thead>
 					<tr>
-						<td><?php echo $no++ ?></td>
-						<td><?php echo $row->nim?></td>
-						<td><?php echo $row->nama?></td>
-						<td><button type="submit" class="btn btn-warning btn-sm">edit</button><button type="delete" class="btn btn-danger btn-sm">delete</button></td>
-					</tr>				
-				<?php } ?>
-			</tbody>
-		</table>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Tabel GAP Kandungan Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Tabel GAP Kandungan Non Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Ketentuan Pembobotan</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>Selisih</th>
+						<th>Bobot Nilai</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Angka</td>
+						<td>Nilai</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Pembobotan Kandungan Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Pembobotan Kandungan Non Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Perhitungan Factor Kandungan Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Perhitungan Factor Kandungan Non Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Perhitungan Nilai Total Kandungan Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Perhitungan Nilai Total Kandungan Non Asam</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<br>
+		<div>
+			<h2>Ranking</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>Ranking</th>
+						<th>Nama Alternatif</th>
+						<th>Nilai Total</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>No</td>
+						<td>Nama</td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
 <?php $this->load->view('layout/footer');?>
 

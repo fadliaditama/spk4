11e0745e3a047da996dc073720b6394dca7bc5aa
Acmad Fadli Aditama <37434467+fadliaditama@users.noreply.github.com>
2019-12-04 11:33:25 +0700

    add about
    untuk pengenalan kasus

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<?php $this->load->view('layout/header');?>
+
+<div class="container">
+	<h1 align="center">Penentuan Prioritas Jenis Wine Berdasarkan Kandungan yang Mempengaruhi Kesehatan Gigi dengan Metode Profile Matching</h1>
+	<hr class="my-4">	
+	<p>Penelitian yang dilakukan di Universitas Adelaide telah mengungkapkan, tingkat keasaman dalam minuman anggur atau ‘wine’ bisa menyebabkan kerusakan gigi. Sebuah studi menunjukkan, mencicipi minuman ‘wine’ selama 10 menit bisa menimbulkan pelunakan enamel gigi. Cara menikmati Wine yang terbaik adalah dengan menyeruputnya secara perlahan. Namun, semakin lama Wine berkontak dengan lapisan email gigi, maka stain dan pengikisan akan semakin terjadi. Oleh karena itu, jangan biarkan Wine menggenang terlalu lama di dalam mulut.  Namun dalam penelitian ini, kami akan melakukan penyortiran  terhadap jenis wine yang ada di dataset dan menentukan wine terbaik untuk di konsumsi yang tingkat ‘merusak gigi’-nya lebih kecil.</p>
+	<div class="col-sm-4">
+		<ul class="list-group list-group-flush">
+	  	<li class="list-group-item active">Daftar Anggota</li>
+	  	<li class="list-group-item">Acmad Fadli Aditama (09021181621020)</li>
+	 	<li class="list-group-item">Destia Asri Felliani (09021181621014)</li>
+	  	<li class="list-group-item">Kartika Rahmayani (09021281621042)</li>
+	 	<li class="list-group-item">Veti Vera (09021181621142)</li>
+	</ul>
+	</div>
+	
+	<hr class="my-4">
+	<p>	Program ini dibuat untuk memenuhi UAS mata kuliah Sistem Pendukung Keputusan.</p>
+</div>
+
+
+<?php $this->load->view('layout/footer');?>
